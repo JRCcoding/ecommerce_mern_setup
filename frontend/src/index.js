286c3e23b00c8ad5bb9ad25ca031f2b8e3ae0891
Josh Claxton <jrccode@outlook.com>
2022-11-04 00:00:14 -0500

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-
+//test
 ReactDOM.render(
   <Provider store={store}>
     <App />
