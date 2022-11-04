45a9a5a31035509bc054d821dd60536410817915
Josh Claxton <jrccode@outlook.com>
2022-11-03 22:35:00 -0500

    heroku prep

---
+++ backend/data/products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    name: 'Moroccan Mint',
+    image: '/images/moroccanmint.png',
+    description:
+      'This soap contains scent notes mixed of spearmint, peppermint, cornmint, and a touch of black tea.',
+    category: 'Artisan Soap',
+    price: 6.99,
+    countInStock: 12,
+  },
+  {
+    name: 'Black Amber Musk',
+    image: '/images/blackambermusk.png',
+    description:
+      'This soap contains scent notes mixed of lemon, lime, French lavender, woods, amber, and white musk.',
+    category: 'Artisan Soap',
+    price: 6.99,
+    countInStock: 10,
+  },
+]
+
+export default products
