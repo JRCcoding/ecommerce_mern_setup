32d203948a822bc6f66640d14dbc48059e08d9ad
Josh Claxton <jrccode@outlook.com>
2022-11-04 14:02:02 -0500

    nav dark dark

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg='primary' variant='light' expand='lg' collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
             <Navbar.Brand>Stony Hill Essentials</Navbar.Brand>
