5ed18a1ab5865176b6ffd82955c173c030a2a4ca
Josh Claxton <jrccode@outlook.com>
2022-11-05 13:57:02 -0500

    shipping 15 if under 100

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -26,7 +26,8 @@ const PlaceOrderScreen = ({ history }) => {
   cart.itemsPrice = addDecimals(
     cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   )
-  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100)
+  //SHIPPING COST!!!!!
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 15)
   cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
   cart.totalPrice = (
     Number(cart.itemsPrice) +
