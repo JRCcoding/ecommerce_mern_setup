b6b243b76f0f24aa5ff62a603ede5494bf279347
Josh Claxton <jrccode@outlook.com>
2022-11-06 12:24:08 -0600

    searchbox flex

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -14,7 +14,7 @@ const SearchBox = ({ history }) => {
   }
 
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} inline className='d-inline-flex'>
       <Form.Control
         type='text'
         name='q'
